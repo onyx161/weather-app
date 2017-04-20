d49bb5360c62b0b09232d5813e762c06f98dff80
Yurii Tykhovskyi <yuri.tykhovskyi@gmail.com>
2017-04-20 10:04:55 +0300

    Requesting Data via HTTP

--- src/app/weather/weather-search.component.ts
+++ src/app/weather/weather-search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms/forms';
 
+import { WeatherItem } from './weather-item';
+import { WeatherService } from './weather.service';
+
 @Component({
   selector: 'wa-weather-search',
   templateUrl: './weather-search.component.html',
@@ -8,13 +11,20 @@ import { NgForm } from '@angular/forms/forms';
 })
 export class WeatherSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _weatherService: WeatherService) { }
 
   ngOnInit() {
   }
 
   onSubmit(f: NgForm) {
     console.log(f);
+    this._weatherService.searchWeatherData(f.value.location)
+      .subscribe(
+        data => {
+          const weatherItem = new WeatherItem(data.name, data.weather[0].description, data.main.temp);
+          this._weatherService.addWeatherItem(weatherItem);
+        }
+      );
   }
 
 }
--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,10 +1,31 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
-import {WEATHER_ITEMS} from './weather.data';
+import { WEATHER_ITEMS } from './weather.data';
+import { WeatherItem } from './weather-item';
 
 @Injectable()
 export class WeatherService {
-    getWeatherItems() {
-        return WEATHER_ITEMS;
-    }
+  private key = '12a9fcd0a3279c818db85a1e60cc83ad';
+
+  constructor(private _http: Http) { }
+
+  getWeatherItems() {
+    return WEATHER_ITEMS;
+  }
+
+  addWeatherItem(weatherItem: WeatherItem) {
+    WEATHER_ITEMS.push(weatherItem);
+  }
+
+  searchWeatherData(cityName: string): Observable<any> {
+    return this._http.get('http://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&APPID=' + this.key + '&units=metric')
+      .map(response => response.json())
+      .catch(error => {
+        console.error(error);
+        return Observable.throw(error.json());
+      });
+  }
 }
