8143452ae50a466df39eb2797982879d7dd6f73e
Yurii Tykhovskyi <yuri.tykhovskyi@gmail.com>
2017-04-21 11:37:51 +0300

    Added profiles functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,25 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { WeatherListComponent } from './weather/weather-list.component';
 import { WeatherItemComponent } from './weather/weather-item.component';
-import { WeatherService } from 'app/weather/weather.service';
 import { WeatherSearchComponent } from './weather/weather-search.component';
+import { SidebarComponent } from './sidebar.component';
+import { WeatherService } from 'app/weather/weather.service';
+import { ProfileService } from 'app/profile.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherListComponent,
     WeatherItemComponent,
-    WeatherSearchComponent
+    WeatherSearchComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [WeatherService],
+  providers: [WeatherService, ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/sidebar.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+import { WeatherService } from 'app/weather/weather.service';
+import { WeatherItem } from 'app/weather/weather-item';
+import { ProfileService } from "app/profile.service";
+import { Profile } from "app/profile";
+
+@Component({
+  selector: 'wa-sidebar',
+  template: `
+    <h3>Saved Profiles</h3>
+    <button (click)="onSaveNew()">Save List to Profile</button>
+    <article class="profile" *ngFor="let profile of profiles" (click)="onLoadProfile(profile)">
+      <h4>{{ profile.profileName }}</h4>
+      <p>Cities: {{ profile.cities.join(', ') }}</p>
+      <span class="delete" (click)="onDeleteProfile($event, profile)">X</span>
+    </article>
+    `,
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  profiles: Profile[];
+
+  constructor(private _profileService: ProfileService, private _weatherService: WeatherService) { }
+
+  onSaveNew() {
+    const cities = this._weatherService.getWeatherItems().map(function (element: WeatherItem) {
+      return element.cityName;
+    });
+    this._profileService.saveNewProfile(cities);
+  }
+
+  onLoadProfile(profile: Profile) {
+    this._weatherService.clearWeatherItems();
+    for (let i = 0; i < profile.cities.length; i++) {
+      this._weatherService.searchWeatherData(profile.cities[i])
+        .retry()
+        .subscribe(
+        data => {
+          const weatherItem = new WeatherItem(data.name, data.weather[0].description, data.main.temp);
+          this._weatherService.addWeatherItem(weatherItem);
+        }
+        );
+    }
+  }
+
+  onDeleteProfile(event: Event, profile: Profile) {
+    event.stopPropagation();
+    this._profileService.deleteProfile(profile);
+  }
+
+  ngOnInit() {
+    this.profiles = this._profileService.getProfiles();
+  }
+
+}
